c4b6c0ee5aee76a3f48860b7512de165f4042c67
Dima <gubatenkod@icloud.com>
2024-01-13 13:15:59 +0200

    updated ci

--- ci_build.sh
+++ ci_build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --name dna_game_container --rm -v unreal_engine_5.3.2:/tmp/unreal_engine -v $(pwd)/Project:/tmp/src -v $(pwd)/archive:/tmp/archive azzulis/dnagame_build:2.3 /bin/bash /tmp/build.sh DNAProject Shipping Linux /tmp/archive /tmp/stage
+docker run --name dna_game_container --rm -v unreal_engine_5.3.2:/tmp/unreal_engine -v $(pwd)/Project:/tmp/src -v $(pwd)/archive:/tmp/archive azzulis/dnagame_build:2.4 /bin/bash /tmp/build.sh DNAProject Shipping Linux /tmp/archive /tmp/stage
