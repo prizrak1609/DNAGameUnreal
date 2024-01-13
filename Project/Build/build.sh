5101dfd73d4c6ead1b2a689180105339726a5875
Dima <gubatenkod@icloud.com>
2024-01-13 18:09:25 +0200

    final change, arhiving CI temporary

--- Project/Build/build.sh
+++ Project/Build/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+
 export ProjectPath=$1
 export Configuration=$2
 export TargetPlatform=$3
@@ -7,10 +9,45 @@ export ArchiveDir=$4
 export StageDir=$5
 
 sudo chown -R builder /tmp/unreal_engine
+sudo chown -R builder /tmp/src
+sudo chown -R builder /tmp/archive
 
 cd /tmp
 
-./unreal_engine/Engine/Build/BatchFiles/RunUAT.sh BuildCookRun \
+ls -al
+
+ls -al src
+
+pwd
+
+echo "/tmp/unreal_engine/Engine/Build/BatchFiles/RunUAT.sh BuildCookRun \
+            -project=$ProjectPath \
+            -build \
+            -configuration=$Configuration \
+            -targetplatform=$TargetPlatform \
+            -cook \
+            -unversionedcookedcontent \
+            -stage \
+            -stagingdirectory=$StageDir \
+            -pak \
+            -compressed \
+            -prereqs \
+            -archive \
+            -archivedirectory=$ArchiveDir \
+            -archivemetadata \
+            -package \
+            -allmaps \
+            -utf8output \
+            -buildmachine \
+            -unattended \
+            -noP4 \
+            -nosplash \
+            -stdout \
+            -NoCodeSign \
+            -separatedebuginfo \
+            -deploy"
+
+bash /tmp/unreal_engine/Engine/Build/BatchFiles/RunUAT.sh BuildCookRun \
             -project=$ProjectPath \
             -build \
             -configuration=$Configuration \
