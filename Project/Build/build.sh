c629d0ac36b4f593eb92cecbd6ce5946b5570852
Dima <gubatenkod@icloud.com>
2024-01-13 17:17:31 +0200

    updated build

--- Project/Build/build.sh
+++ Project/Build/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export ProjectName=$1
+export ProjectPath=$1
 export Configuration=$2
 export TargetPlatform=$3
 export ArchiveDir=$4
@@ -9,7 +9,7 @@ export StageDir=$5
 sudo chown -R builder /tmp/unreal_engine
 
 /tmp/unreal_engine/Engine/Build/BatchFiles/RunUAT.sh BuildCookRun \
-            -project=/tmp/src/$ProjectName.uproject \
+            -project=$ProjectPath \
             -build \
             -configuration=$Configuration \
             -targetplatform=$TargetPlatform \
